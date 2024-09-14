7bf9a115fedad4452ddc71dad00a20bb7f6e0cf4
Jhong <ghddl23@naver.com>
2024-09-14 12:07:37 +0900

    Feat : change component (class to function)

--- new-todolist/src/App.js
+++ new-todolist/src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
-function App() {
+export default function App() {
+  const [todoData, setTodoData] = useState([]);
+  const [value, setValue] = useState('');
+
+  const btnStyle = {
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+  }
+
+  const getStyle = (completed) => {
+    return {
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: completed ? 'line-through' : 'none'
+    }
+  }
+
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter(data => data.id !== id)
+    setTodoData(newTodoData);
+  }
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
+
+    setTodoData(prev => [...prev, newTodo]);
+    setValue('');
+  }
+
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map(data => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    })
+    setTodoData(newTodoData);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='container'>
+      <div className='todoBlock'>
+        <div className='title'>
+          <h1>할 일 목록</h1>
+        </div>
+        {todoData.map((data) => (
+          <div style={getStyle(data.completed)} key={data.id}>
+            <input type='checkbox' defaultChecked={data.completed} onChange={() => handleCompleteChange(data.id)} />
+            {data.title}
+            <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
+          </div>
+        ))}
 
-export default App;
+        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='value'
+            style={{ flex: '10', padding: '5px' }}
+            placeholder='해야 할 일을 입력하세요.'
+            value={value}
+            onChange={handleChange}
+          />
+          <input
+            type='submit'
+            value='입력'
+            className='btn'
+            style={{ flex: '1' }}
+          />
+        </form>
+      </div>
+    </div>
+  )
+}
